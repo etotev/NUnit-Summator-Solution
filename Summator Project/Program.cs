0052b7a26d9a1f53d6962a8051f6577dc949928a
evelin.totev <evelin.totev@EGT-196-LT>
2022-05-13 12:51:12 +0300

    Add project files.

---
+++ Summator Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new int[] { 2, 2, 3, 4, 5, 6 });
+        }
+    }
+}
---
+++ Summator.Tests/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿namespace Summator.Tests
+{
+    public class Tests
+    {
+        [Test]        // Задължително се пише, иначе не излиза в Test Explorer
+        
+        public void Test_Sum_TwoPossitiveNumbers()
+        {
+            long result = Summator.Sum(new int[] { 5, 7 }); // Задължително се прави референция към Summator Project
+
+            int expected = 12;
+
+            Assert.That(result == expected);
+        }
+
+        [Test]
+
+        public void Test_Sum_ThreePossitiveNumbers()
+        {
+            long result = Summator.Sum(new int[] { 5, 7 , 5 });
+
+            int expected = 17;
+
+            Assert.That(result == expected);
+        }
+
+        [Test]
+
+        public void Test_Sum_TwoNegativeNumbers()
+        {
+            long result = Summator.Sum(new int[] { -7,-9 });
+
+            int expected = -16;
+
+            Assert.That(result == expected);
+        }
+
+        [Test]
+
+        public void Test_Sum_TwoZeroes()
+        {
+            long result = Summator.Sum(new int[] { 0,0 });
+
+            int expected = 0;
+
+            Assert.That(result == expected);
+        }
+
+        [Test]
+
+        public void Test_Sum_BigValues()
+        {
+            long result = Summator.Sum(new int[] { 3000000000, 5000000000 });
+
+            long expected = 8000000000;
+
+            Assert.That(result == expected);
+
+        }
+    }
+}
